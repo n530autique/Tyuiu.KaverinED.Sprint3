bac2ee8b7bc86c9522c1918de9a25ed15fbfdb43
Каверин Егор <Tyuiu.KaverinED@outlook.com>
2023-10-18 16:56:14 +0500

    Task3

---
+++ Tyuiu.KaverinED.Sprint3.Task3.V15.Lib/DataService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.KaverinED.Sprint3.Task3.V15.Lib
+{
+    public class DataService : ISprint3Task3V15
+    {
+        public int GetMinCharCount(string value, char item)
+        {
+            int res = -1;
+            int counter = 0;
+            foreach (char m in value) { }
+            for (int i = 1; i < value.Length; i++)
+            {
+                if (value[i - 1] == item)
+                {
+                    counter++;
+                }
+                else
+                {
+                    res = counter > res ? counter : res;
+                    counter = 0;
+                }
+            }
+            
+            return res;
+        }
+    }
+}
+
